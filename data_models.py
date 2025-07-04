ede5a41d31e70ac39e7c4e13d97cd51d3a426222
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-07-04 13:44:33 +0200

    update Book model: isbn nullable=True (isbn might not always available)

--- data_models.py
+++ data_models.py
@@ -38,7 +38,7 @@ class Book(db.Model):
     __tablename__ = 'books'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    isbn = db.Column(db.Integer, nullable=False)
+    isbn = db.Column(db.Integer, nullable=True)
     title = db.Column(db.String, nullable=False)
     publication_year = db.Column(db.Integer, nullable=True)
 
